6181a1cb64c1426bbd572b2dd73ab8f992796c6f
mo.taheri <mo.taheri@mahsan.co>
2021-05-19 18:36:55 +0430

    update cv add medium

--- src/components/experience/experience.js
+++ src/components/experience/experience.js
@@ -3,24 +3,33 @@ export const exp = [
     name: "Mahsan",
     url: "https://www.linkedin.com/company/mahsan/",
     position: "Frontend Developer",
-    date: "Jul 2020 - present"
+    date: "Jul 2020 - March 2021",
+    description: [
+      "We have worked on the Winna web application  and we were responsible for creating  features , services and etc  with  Angular framework  we had Unit test for all of the components and services ,Winna is web application for monitoring and provides fully controlled access to an organization’s assets  ",
+    ],
   },
   {
     name: "Delino",
     url: "https://www.linkedin.com/company/delino/",
     position: "Frontend Developer",
     date: "Feb 2019 - Aug 2019",
-    description: ["I worked with react for implement a core for making delivery food", "Create Store in App with Redux"]
+    description: [
+      "I worked with react for implement a core for making delivery food",
+      "Create Store in App with Redux",
+    ],
   },
   {
     name: "BestMeet",
     position: "Frontend Developer",
     date: "Jul 2018 - Sep 2018",
-    description: ["I was intern of frontend in that Start-up", "I started frontend from there and I learned Bootstrap 4"]
+    description: [
+      "I was intern of frontend in that Start-up",
+      "I started frontend from there and I learned Bootstrap 4",
+    ],
   },
   {
     name: "DPI co",
     position: "Wordpress Developer",
-    date: "Jan 2018 – Feb 2018"
-  }
+    date: "Jan 2018 – Feb 2018",
+  },
 ]
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -3,24 +3,45 @@ import Linkedin from "../../../assets/linkedin.svg"
 import Twitter from "../../../assets/twitter.svg"
 import Telegram from "../../../assets/telegram.svg"
 import Github from "../../../assets/github.svg"
+import Medium from "../../../assets/medium.svg"
 
 import "./style.scss"
 
 const Header = () => {
   return (
     <div className="header__profile">
-      <div className="image">
-      </div>
+      <div className="image"></div>
       <div className="header__info">
         <span className="name">Mohammad Taheri</span>
         <ul className="header__pro">
           <li>mohammadtaheri506@gmail.com</li>
           <li>+989375050156</li>
           <li className="icons">
-            <div><a href="https://www.linkedin.com/in/tmohammad78/"><Linkedin /></a></div>
-            <div><a href="https://twitter.com/tmohammad78"><Twitter /></a></div>
-            <div><a href="https://github.com/tmohammad78"><Github /></a></div>
-            <div><a href="https://t.me/Tmohammad_taheri"><Telegram /></a></div>
+            <div>
+              <a href="https://www.linkedin.com/in/tmohammad78/">
+                <Linkedin />
+              </a>
+            </div>
+            <div>
+              <a href="https://mohammadtaheri.medium.com/">
+                <Medium />
+              </a>
+            </div>
+            <div>
+              <a href="https://twitter.com/tmohammad78">
+                <Twitter />
+              </a>
+            </div>
+            <div>
+              <a href="https://github.com/tmohammad78">
+                <Github />
+              </a>
+            </div>
+            <div>
+              <a href="https://t.me/Tmohammad_taheri">
+                <Telegram />
+              </a>
+            </div>
           </li>
         </ul>
       </div>
